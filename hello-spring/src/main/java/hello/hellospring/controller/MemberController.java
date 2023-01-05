5857e8cbe9b25625c63988543f60f9ace426362f
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2023-01-05 11:25:06 +0900

    registration mamber

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,7 +2,10 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -13,5 +16,20 @@ public class MemberController {
     @Autowired // Spring 컨테이너에 등록
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm"; // template에서 찾음
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        
+        return "redirect:/";
     }
 }
