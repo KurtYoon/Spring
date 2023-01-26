92ebf57c20355e79c2fb9898764ab8b547df1f6a
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2023-01-27 02:34:06 +0900

    AOP

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,36 +1,18 @@
 package hello.hellospring;
-
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import hello.hellospring.app.TimeTraceAOP;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.service.MemberService;
-
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
-
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
 }
