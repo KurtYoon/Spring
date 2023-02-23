3ed12ad2e52d71db2636f3083e7f88d006c0a3a7
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2023-02-24 04:54:05 +0900

    clear basic

--- Spring Basic/core/src/main/java/hello/core/OrderApp.java
+++ Spring Basic/core/src/main/java/hello/core/OrderApp.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
+
     public static void main(String[] args) {
 
 //        AppConfig appConfig = new AppConfig();
