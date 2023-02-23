3b8325a0a7f529d3ce544894a2103e74620a3d91
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2023-02-24 00:13:23 +0900

    make annotation

--- Spring Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,24 +1,27 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final에 대한 생성자를 만들어 줌
+//@RequiredArgsConstructor // final에 대한 생성자를 만들어 줌
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
 
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 추상화와 구체 모두에 의존함
