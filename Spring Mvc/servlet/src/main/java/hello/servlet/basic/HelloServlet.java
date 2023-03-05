8fbc449656876f560f923bbda985254d1173c95d
KurtYoon <100412714+KurtYoon@users.noreply.github.com>
2023-03-06 02:12:35 +0900

    servlet

--- Spring Mvc/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ Spring Mvc/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,18 +1,27 @@
 package hello.servlet.basic;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet extends HttpServlet {
+public class HelloServlet  extends HttpServlet {
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); // 컨텐츠 타입
+        response.setCharacterEncoding("utf-8"); // 인코딩
+        response.getWriter().write("hello " + username); // http 메시지 바디에 적음
     }
 }
